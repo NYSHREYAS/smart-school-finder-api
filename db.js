e281f8e5bd8ca79a6314f9935031b83d72940498
Shreyas Nepale <123090523+NYSHREYAS@users.noreply.github.com>
2026-04-17 23:36:04 +0530

    Refactor DB connection settings in db.js
    Updated database connection configuration to include port and SSL options.

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "your_password",
-    database: process.env.DB_NAME || "schoolDB"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("DB Connection Error:", err.message);
-    } else {
-        console.log("MySQL Connected ✅");
-    }
+  if (err) {
+    console.error("DB Connection Error:", err.message);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
 });
 
 module.exports = db;
+
